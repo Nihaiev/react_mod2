86c818b98ff00ed98df82993d46104c9b91ee1ee
Ivan <nigaev.ivan@gmail.com>
2022-10-25 23:30:48 +0300

    state hosting, lifting state up

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Counter from './conponents/Counter/Counter';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#f44336' },
+  { label: 'green', color: '#4caf50' },
+  { label: 'blue', color: '#2196f3' },
+  { label: 'grey', color: '#607d88' },
+  { label: 'pink', color: '#e91e63' },
+  { label: 'indigo', color: '#3f51b5' },
+];
+
+const App = () => (
+  <>
+    <h1>Стан компонента</h1>
+    <Counter initialValue={10} />
+  </>
+);
+
+export default App;
+
+//56
