57091d4320aaaf5471ab6a4dbd85853a781cce7e
Ivan <nigaev.ivan@gmail.com>
2022-10-26 15:44:09 +0300

    Dropdown

---
+++ src/conponents/Dropdown/Dropdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Dropdown.css';
+
+class Dropdown extends Component {
+  render() {
+    return (
+      <div className="Dropdown">
+        <button type="button" className="Dropdown__toggle"></button>
+      </div>
+    );
+  }
+}
